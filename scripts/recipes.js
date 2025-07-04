e701ac984b37785383d70874452bc40811ac48d6
Francesco Del Colombo <francescodelcolombo2@gmail.com>
2025-07-04 11:38:35 +0200

    First Commit ToDoList

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -7,16 +7,20 @@ function loadRecipes(page) {
   fetch('https://dogapi.dog/api/v2/breeds',{
     method: "GET",
   })
-  
-    .then((response) => response.json())
-    .then((data) => {
-      const container = document.getElementById("recipes-container");
-      const ul = document.createElement("ul");
-      container.innerHTML = "";
 
-      data.results.forEach((recipe) => {
+    .then((response) => response.json())
+    .then((result)=>{
+
+        console.log(result);
+        console.log(result.data);
+        const container = document.getElementById("recipes-container");
+        const ul = document.createElement("ul");
+        container.innerHTML ="";
+    });
+
+      result.data.forEach((item) => {
         const li = document.createElement("li");
-        li.innerHTML = `<h3>${recipe.title}</h3>`;
+        li.innerHTML = `<h3>${item.attributes.name}</h3>`;
         ul.appendChild(li);
       });
 
